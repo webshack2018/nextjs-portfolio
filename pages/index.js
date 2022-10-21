3570af9398a0c8822076eeb4e3a444f438546fd5
DEBA <54439717+webshack2018@users.noreply.github.com>
2022-10-21 16:02:35 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -48,9 +48,7 @@ export default function Home() {
 
             </p>
             
-            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
-              <Image src={deved} layout="fill" objectFit="cover" />
-            </div>
+            
           </div>
         </section>
 
